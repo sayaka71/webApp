c717ba9e0660231b0bb5eea28b9f893ccac6845f
sayaka71 <saya.56k@gmail.com>
2022-08-31 05:50:29 +0900

    非同期通信async/await

--- ui/test-01/js/script.js
+++ ui/test-01/js/script.js
@@ -1,8 +1,13 @@
 // JavaScriptでwebAPIを叩いてみる
 // https://qiita.com/anago1030/items/5292007ed5db10d25505
-function callApi() {
-  const res = fetch("https://jsonplaceholder.typicode.com/users");
-  console.log(res)
+// fetch関数を実行するとPromissというオブジェクトが返ってきました。
+// Promissとは非同期処理の操作が完了したときに結果を返すものです。 非同期処理とは、ある処理が実行されてから終わるまで待たずに、次に控えている別の処理を行うことです。
+
+async function callApi() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  console.log(res);
+  console.log(users);
 };
 
 callApi();
